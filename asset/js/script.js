496ef10310e1ec8124d4ae4b6d27508dea540d09
Noyon-Mahmud <noyonweb5686@gmail.com>
2023-11-30 03:10:10 +0600

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    'use strict';
+
+    // banner - Slider 
+    $('.banner_img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+      });
+
+});
+
+
+// ================
